32adf973e0ac4f17233d65e19483befe598a5bb1
Alex <alex@debian.local>
2026-05-05 10:10:57 +0300

    service: align paper safe symbols with current portfolio

--- scripts/run_paper_safe.sh
+++ scripts/run_paper_safe.sh
@@ -9,7 +9,7 @@ set +a
 
 exec env PYTHONPATH=src venv/bin/python -u src/scripts/run_market_pipeline.py \
   --symbol BRM6@RTSX \
-  --symbols BRM6@RTSX,SBER@MISX,PLZL@MISX,NGH6@RTSX,LKOH@MISX \
+  --symbols BRM6@RTSX,USDRUBF@RTSX,SBERP@MISX,PLZL@MISX,LKOH@MISX,VTBR@MISX,NVTK@MISX,X5@MISX,SFIN@MISX,OZON@MISX,EUTR@MISX,T@MISX,NGH6@RTSX \
   --strategy vwap_bands_mr \
   --run-secs 0 \
   --quote-log-every 30 \
