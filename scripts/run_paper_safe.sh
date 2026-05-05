72eec35745514fd7714eab3c394eb71e95f30405
tammat <2670487+tammat@users.noreply.github.com>
2026-05-05 14:47:10 +0300

    service: switch gas contract to NGK6

--- scripts/run_paper_safe.sh
+++ scripts/run_paper_safe.sh
@@ -9,7 +9,7 @@ set +a
 
 exec env PYTHONPATH=src venv/bin/python -u src/scripts/run_market_pipeline.py \
   --symbol BRM6@RTSX \
-  --symbols BRM6@RTSX,USDRUBF@RTSX,SBERP@MISX,PLZL@MISX,LKOH@MISX,VTBR@MISX,NVTK@MISX,X5@MISX,SFIN@MISX,OZON@MISX,EUTR@MISX,T@MISX,NGH6@RTSX \
+  --symbols BRM6@RTSX,USDRUBF@RTSX,SBERP@MISX,PLZL@MISX,LKOH@MISX,VTBR@MISX,NVTK@MISX,X5@MISX,SFIN@MISX,OZON@MISX,EUTR@MISX,T@MISX,NGK6@RTSX \
   --strategy vwap_bands_mr \
   --run-secs 0 \
   --quote-log-every 30 \
