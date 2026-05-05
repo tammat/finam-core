01e387aedea6677e597168e2dca398b33d08b56a
tammat <2670487+tammat@users.noreply.github.com>
2026-05-05 09:36:34 +0300

    service: add LKOH to paper safe symbols

--- scripts/run_paper_safe.sh
+++ scripts/run_paper_safe.sh
@@ -9,7 +9,7 @@ set +a
 
 exec env PYTHONPATH=src venv/bin/python -u src/scripts/run_market_pipeline.py \
   --symbol BRM6@RTSX \
-  --symbols BRM6@RTSX,SBER@MISX,PLZL@MISX,NGH6@RTSX \
+  --symbols BRM6@RTSX,SBER@MISX,PLZL@MISX,NGH6@RTSX,LKOH@MISX \
   --strategy vwap_bands_mr \
   --run-secs 0 \
   --quote-log-every 30 \
